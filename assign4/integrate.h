28b482b19f34eed7f74ab8e06143ca8424e3e7a3
Martik Aghajanian <martik@hotmail.co.uk>
2016-11-28 01:11:30 +0000

    Commented on the code

--- assign4/integrate.h
+++ assign4/integrate.h
@@ -1,36 +1,72 @@
+// Computational Methods Assignment 4 - Numerical Integration
+// Martik Aghajanian Cohort 8
+//
+// Class definition for integration class which takes a user-specified function
+// as well as limits and a user-specified precision to which the method of 
+// integration is expected to converge. This can be used to integrate functions
+// defined on an interval, to within a specified accuracy.
+
 #include <iostream>
 #include <cmath>
 using namespace std;
 
 class Integrate{
+// Class for integrating function "fnc" within limits "a", "b" within given precision "eps"
 private:
+	// For a function "fnc" which takes argument "x", with limits "a" and "b"
+	// iteration the number of points "n" used in the calculation 
+	// in addition to the boundary points. The convergence of the integral is
+	// assessed using double precision float parameter "eps"
 	double (*fnc)(double x);
 	double a;
-	double b;   //should these all be on the same line???
-	double t;	
+	double b;  	
 	int n;
+	double eps;
+
+	// This class continually updates the variables "n" and "evals", the number
+	// function evaluations made in total, upon calling the member function 
+	// "iterate". The variables "I1" and "I2" are updatable values of the integral
+	// values which are compared to assess convergence. The samples taken to 
+	// evaluate the integral are spaced with spacing "h" which is halved upon
+	// each iteration of doubling the number of points used.
 	int evals;
 	double h;
-	double eps;
 	double I2;
 	double I1;
 public:
 	Integrate(double (*func)(double x), double low, double up, double epsilon){
+		// Upon instantiation, three function evaluations are carried out to
+		// provide the estimation for two integrals "I1" and "I2" which form
+		// the first two stages in either the trapezoid or Simpson's rule.
+		// The values of "n" and "h" are updated accordingly.
 		a = low;
 		b = up;
-		h = 0.5*(b-a); // times by two since we perform 2 iterations in initialization
+		h = 0.5*(b-a);
 		n = 1;
 		eps = epsilon;
-		fnc = func;	 //Correct way to pass on functions????
+		fnc = func;
 		I1 = h*((*fnc)(a)+(*fnc)(b));
 		I2 = ((0.5*I1)+((*fnc)(0.5*(a+b))*h));
 		evals = 3;
 	}
 
 	void iterate() {
+		// Member function which iterates the procedure of doubling the number of
+		// points, and calculates the new value of the integral "I2" and compares
+		// it to the previous value "I1" to assess convergence using relative error
+		// "eps". By doubling the number of points "n", the spacing between points
+		// "h" halves.
 		I1 = I2;
 		I2 = 0.5*I1;
 		n *= 2;
+
+		// Instead of recalculating the values at all points whose spacing is half
+		// that of the previous iteration, the previous value is just halved, and 
+		// the extra sampling points intermediate between those previously selected
+		// are calculated and added to the new value of the integral estimation. This
+		// is to minimise the number of function evaluations needed. The number of 
+		// function evaluations "evals" is recorded and updated each time this iterate()
+		// member function is called.
 		for(int i(0); i<n; i++){
 			double x_new = a + (i+0.5)*h;
 			I2 += 0.5*h*(*fnc)(x_new);
@@ -40,6 +76,10 @@ public:
 	}
 
 	double trap(){
+		// Using the iterate() member function, implements the trapezoid rule,
+		// by increasing the number of points sampled in the integration until
+		// the relative change in the integral evaluation upon iteration is
+		// below the required precision.
 		while(abs(I2-I1)>eps*abs(I1)){
 			Integrate::iterate();
 		}
@@ -47,6 +87,11 @@ public:
 	}
 	
 	double simp(){
+		// Using the iterate() member function, implements the Simpson's rule 
+		// by removing 1/3 times the previous evaluation of the trapezoid rule
+		// from 4/3 times the first iteration of the trapezoid rule to get a value
+		// for the integral evaluated using Simpson's rule "s2". This value is then
+		// compared to the previous value using the "eps" parameter.
 		double s1 = (4*I2/3.)-(I1/3.);
 		Integrate::iterate();
 		double s2 = (4*I2/3.)-(I1/3.);
@@ -57,6 +102,10 @@ public:
 		}
 		return s2;
 	}
-	
+		
+	// This member function outputs the number of function evalulations "evals"  
+	// required to determine the integral of "fnc" over interval between "a" and
+	// "b" using either the trapeziod rule or Simpson's rule.
 	int get_evals() { return evals; }
+
 };
--- assign4/numint.cpp
+++ assign4/numint.cpp
@@ -1,16 +1,31 @@
+// Computational Methods Assignment 4 - Numerical Integration
+// Martik Aghajania, Cohort 8
+//
+// Program which estimates the value of the error function, which
+// is the integral from 0 to argument x of the error function 
+// integrand erfi(x), using both the trapezoid rule and Simpson's
+// rule to compare the number of necessary function evaluations.
 #include <iostream>
 #include <cmath>
-#include "integrate.h" // do i need a header for each function?
-// do i need a file for each function?
+#include "integrate.h"
 using namespace std;
 
-double func1(double x) {
+double erfi(double x) {
+	// Given double-preicision float "x", returns the value of the
+	// error function integrand (erfi), using 4*arctan(1) as pi.
         return (2/sqrt(atan(1.0)*4))*exp(-x*x);
 }
 
 int main() {
-        Integrate Result1(&func1, 0, 2, 1e-6);
-	Integrate Result2(&func1, 0, 2, 1e-6);
+	// First this program instantiates two "Integrate" objects which are identical
+	// in setup, but progress using separate methods, one using the trapezoid rule
+	// and one using Simpson's rule, each with function "erfi", a precision 1e-6
+	// and limits from 0 to 2.
+        Integrate Result1(&erfi, 0, 2, 1e-6);
+	Integrate Result2(&erfi, 0, 2, 1e-6);
+
+	// Both values of the integral using the separate methods are displayed, each 
+	// with their respective number of function evaluations for comparison.
 	cout << "Trapezium rule: " << Result1.trap();
 	cout <<  " using " << Result1.get_evals() << " evaluations." << endl;
 	cout << "Simpsons's rule: " << Result2.simp();
