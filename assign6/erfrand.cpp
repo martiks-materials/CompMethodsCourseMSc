98203015943b6efb58b4f70fbd98e1c5560d3de7
Martik Aghajanian <martik@hotmail.co.uk>
2017-01-27 03:39:32 +0000

    Added extra pdf

--- assign6/erfrand.cpp
+++ assign6/erfrand.cpp
@@ -20,45 +20,46 @@ double invcdf(double x){
 }
 
 double pdf2(vector<double> y){
-	return 2*exp(-2*y[0])/(1-pow(M_E, -4));
+	return 0.5*exp(-0.5*y[0])/(1-pow(M_E, -1));
 }
 
 double invcdf2(double x){
-	return -0.5*log(1-((1-pow(M_E,-4))*x));
+	return -2*log(1-((1-pow(M_E,-1))*x));
 }
 
 int main() {
-	bool importan;
-	cout << "Enter 0 for Uniform, 1 for Importance Sampling: ";
-	cin >> importan;
-	cout << endl;
 	int Ndim(1), mstart(500), see(142), maxsteps(22);
 	unsigned long int mmax = int(mstart*pow(2, maxsteps));
 	double eps(1E-6);
         vec a = {0}, b = {2};
-        Mcint Monte = {erfi, a, b, Ndim, see, eps, mstart, importan, pdf, invcdf, mmax};
-        Monte.integrate();
-	if(importan){
-		ofstream outfile1("ertrack2.dat");
-		ofstream outfile2("numtrack2.dat");
-		for(int i(0); i <= maxsteps; i++){
-			outfile1 << log2(Monte.steps[i]/mstart) << "   " << Monte.errors[i] << endl;
-			outfile2 << log2(Monte.steps[i]/mstart) << "   " << Monte.vals[i] << endl;
-		}
-		outfile1.close();
-		outfile2.close();
-        	cout << "Importance Sampling: " << Monte.fsum_new << " +/- " << Monte.error2() <<  endl;
+        Mcint Monte1 = {erfi, a, b, Ndim, see, eps, mstart, false, pdf, invcdf, mmax};
+        Mcint Monte2 = {erfi, a, b, Ndim, see, eps, mstart, true, pdf, invcdf, mmax};
+        Mcint Monte3 = {erfi, a, b, Ndim, see, eps, mstart, true, pdf2, invcdf2, mmax};
+        Monte1.integrate();
+	Monte2.integrate();
+	Monte3.integrate();
+	ofstream outfile1a("ertrack1.dat");
+	ofstream outfile1b("numtrack1.dat");
+	ofstream outfile2a("ertrack2.dat");
+	ofstream outfile2b("numtrack2.dat");
+	ofstream outfile3a("ertrack3.dat");
+	ofstream outfile3b("numtrack3.dat");
+	for(int i(0); i <= maxsteps; i++){
+		outfile1a << log2(Monte1.steps[i]/mstart) << "   " << Monte1.errors[i] << endl;
+		outfile1b << log2(Monte1.steps[i]/mstart) << "   " << Monte1.vals[i] << endl;
+		outfile2a << log2(Monte2.steps[i]/mstart) << "   " << Monte2.errors[i] << endl;
+		outfile2b << log2(Monte2.steps[i]/mstart) << "   " << Monte2.vals[i] << endl;
+		outfile3a << log2(Monte3.steps[i]/mstart) << "   " << Monte3.errors[i] << endl;
+		outfile3b << log2(Monte3.steps[i]/mstart) << "   " << Monte3.vals[i] << endl;
 	}
-	else {
-		ofstream outfile1("ertrack1.dat");
-		ofstream outfile2("numtrack1.dat");
-		for(int i(0); i <= maxsteps; i++){
-			outfile1 << log2(Monte.steps[i]/mstart) << "   " << Monte.errors[i] << endl;
-			outfile2 << log2(Monte.steps[i]/mstart) << "   " << Monte.vals[i] << endl;
-		}	
-		outfile1.close();
-		outfile2.close();
-        	cout << "Uniform Sampling: " << Monte.fsum_new << " +/- " << Monte.error2() <<  endl;
-	}
+	outfile1a.close();
+	outfile1b.close();
+	outfile2a.close();
+	outfile2b.close();
+	outfile3a.close();
+	outfile3b.close();
+        cout << "Uniform Sampling: " << Monte1.fsum_new << " +/- " << Monte1.error2() <<  endl;
+	cout << "Importance Sampling (PDF1): " << Monte2.fsum_new << " +/- " << Monte2.error2() <<  endl;
+	cout << "Importance Sampling (PDF2): " << Monte3.fsum_new << " +/- " << Monte3.error2() <<  endl;
         return 0;
 }
